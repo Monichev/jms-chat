80fa63abdb4820dccd6b926483d87dcc054ed1ed
Sergey Monichev <sergey.monichev@gmail.com>
2019-03-09 23:34:50 +0300

    Remove cachingJmsConnectionFactory error

--- client/src/main/java/com/monichev/jmschat/client/ClientApplication.java
+++ client/src/main/java/com/monichev/jmschat/client/ClientApplication.java
@@ -1,9 +1,10 @@
 package com.monichev.jmschat.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ActiveMQAutoConfiguration.class)
 public class ClientApplication {
 
     public static void main(String[] args) {
